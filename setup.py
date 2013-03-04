164817187fec1a1f2e66fc6c698abda688576ed1
Chris Barnes <senrabc@gmail.com>
2013-03-04 16:26:34 -0500

    setup empty project structure for helloWord.py and included files needed for packaging.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='helloWorld',
+    version='0.1dev',
+    packages=['helloWorld',],
+    license='3-clause BSD License',
+    long_description=open('README').read(),
+)
